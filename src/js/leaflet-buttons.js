3ae8a6a33f825b69caf06d083debf4d4b2a4f149
Simon Prévidente <simon.previdente@free.fr>
2015-07-16 10:40:00 +0200

    Refact: Move maxBounds from leaflet-buttons to leaflet-maps

--- src/js/leaflet-buttons.js
+++ src/js/leaflet-buttons.js
@@ -1,13 +1,6 @@
 (function(global) {
 	'use strict';
 
-	// maxBounds
-	var max_bounds_strict = new L.LatLngBounds(new L.LatLng(46.86008, -2.55754), new L.LatLng(47.83486, -0.92346));
-	var max_bounds_buffer = new L.LatLngBounds(new L.LatLng(46.8, -3.0), new L.LatLng(47.87, -0.8));
-	map1.setMaxBounds(max_bounds_buffer);
-	map2.setMaxBounds(max_bounds_buffer);
-
-
 	// Border
 	var loire_atlantique_json = {
 		"type": "FeatureCollection",
--- src/js/leaflet-maps.js
+++ src/js/leaflet-maps.js
@@ -31,6 +31,12 @@
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map2);
 
+	// maxBounds
+	var max_bounds_strict = new L.LatLngBounds(new L.LatLng(46.86008, -2.55754), new L.LatLng(47.83486, -0.92346));
+	var max_bounds_buffer = new L.LatLngBounds(new L.LatLng(46.8, -3.0), new L.LatLng(47.87, -0.8));
+	map1.setMaxBounds(max_bounds_buffer);
+	map2.setMaxBounds(max_bounds_buffer);
+
 	randomDisplay();
 
 })(this);
